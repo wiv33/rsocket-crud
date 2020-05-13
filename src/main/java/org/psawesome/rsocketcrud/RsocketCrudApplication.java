83ef166a8623280a6d06db21cb225875c5b40169
PSK <wiv821253@gmail.com>
2020-05-13 15:13:27 +0900

    Initial commit

---
+++ src/main/java/org/psawesome/rsocketcrud/RsocketCrudApplication.java
@@ -0,0 +1,13 @@
+package org.psawesome.rsocketcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsocketCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsocketCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/psawesome/rsocketcrud/RsocketCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.psawesome.rsocketcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsocketCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
